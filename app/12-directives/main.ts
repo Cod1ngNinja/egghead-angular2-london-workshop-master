2afcd66da4b08798b5107d54ac027a66e0d22358
Klas Mellbourn <klas@mellbourn.net>
2015-10-19 17:21:39 +0100

    last app not working

---
+++ app/12-directives/main.ts
@@ -0,0 +1,25 @@
+import {Component, bootstrap, Directive, HostListener, HostBinding} from "angular2/angular2";
+
+@Directive({
+	selector: '[hi]'
+})
+class Hi{
+	@HostListener('click') onClick() {
+		console.log("clicked on the host", this.message);
+	}
+	
+	@HostBinding('innerHTML') message = '<div>Awesome</div>';
+}
+
+@Component({
+    selector: `app`,
+	directive: [Hi],
+    template: `
+		<div hi>
+			Hello World
+		</div>
+	`
+})
+class App {}
+
+bootstrap(App);
