62a406fa84d19a5d6ccab7739d0fae7700cfad45
Klas Mellbourn <klas@mellbourn.net>
2015-10-19 12:39:12 +0100

    added mouse

--- exercise/02-components-events-properties/main.ts
+++ exercise/02-components-events-properties/main.ts
@@ -1,11 +1,15 @@
 import {Component, bootstrap} from "angular2/angular2";
 import {Computer} from "./computer";
+import {Mouse} from "./mouse";
 
 @Component({
 	selector: 'app',
-	directives: [Computer],
+	directives: [Computer, Mouse],
 	template: `
-	   <div><computer></computer></div>
+	   <div>
+	     <computer></computer>
+	     <mouse></mouse>
+	   </div>
 	`
 })
 class App{
---
+++ exercise/02-components-events-properties/mouse.ts
@@ -0,0 +1,9 @@
+import {Component, Input, Output, EventEmitter} from "angular2/angular2";
+
+@Component({
+	selector: "mouse",
+	template: `I'm a mouse`
+})
+export class Mouse {
+	constructor() {}
+}
