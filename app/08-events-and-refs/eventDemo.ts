d3abee14f9248df546df3f38acf45935fb99a45d
Klas Mellbourn <klas@mellbourn.net>
2015-10-19 12:15:38 +0100

    input with local variable

--- app/08-events-and-refs/eventDemo.ts
+++ app/08-events-and-refs/eventDemo.ts
@@ -2,14 +2,16 @@ import {Component} from "angular2/angular2";
 
 @Component({
 	selector:'event-demo',
+	// parens give event bidning
 	template:`
 	<div>
-	  <button (click)="onClick()">ClickMe</button>
+	  <input #name-input type="text">
+	  <button (click)="onClick(nameInput.value)">ClickMe</button>
 	</div>
 	`
 })
 export class EventDemo{
-	onClick() {
-		console.log("clicked");
+	onClick(name) {
+		console.log(name);
 	}
 }
