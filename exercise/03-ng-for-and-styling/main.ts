c0c824ede29d80065f904889f74234475965787b
Klas Mellbourn <klas@mellbourn.net>
2015-10-19 15:36:32 +0100

    table component

---
+++ exercise/03-ng-for-and-styling/main.ts
@@ -0,0 +1,15 @@
+import {Component, bootstrap} from "angular2/angular2";
+import {Table} from "./table"
+
+@Component({
+    selector: `app`,
+	directives: [Table],
+    template: `
+		<table>
+			
+		</table>
+	`
+})
+class App {}
+
+bootstrap(App);
---
+++ exercise/03-ng-for-and-styling/table.ts
@@ -0,0 +1,8 @@
+import {Component} from "angular2/angular2"
+import {starwarsPeople} from "./starwarsPeople";
+
+@Component({
+	selector: 'table',
+	templateUrl: 'table.html'
+})
+export class Table{}
