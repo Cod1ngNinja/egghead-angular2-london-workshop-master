b4d7ee6db062948d65eab762aa63ffcd4d1ad874
Klas Mellbourn <klas@mellbourn.net>
2015-10-19 12:21:41 +0100

    databinding value

--- app/08-events-and-refs/eventDemo.ts
+++ app/08-events-and-refs/eventDemo.ts
@@ -3,14 +3,16 @@ import {Component} from "angular2/angular2";
 @Component({
 	selector:'event-demo',
 	// parens give event bidning
+	// square brackets can be removed, then it will just set it to the string value
 	template:`
 	<div>
-	  <input #name-input type="text">
+	  <input #name-input type="text" [value]="title">
 	  <button (click)="onClick(nameInput.value)">ClickMe</button>{{nameInput.value}}
 	</div>
 	`
 })
 export class EventDemo{
+	title = "Back to the future";
 	onClick(name) {
 		console.log(name);
 	}
