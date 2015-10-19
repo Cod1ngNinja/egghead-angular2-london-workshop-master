69470a354d41578a35796400539ee209fe92117d
Klas Mellbourn <klas@mellbourn.net>
2015-10-19 14:33:16 +0100

    added injectable declaration

--- app/09-services/foodService.ts
+++ app/09-services/foodService.ts
@@ -1,3 +1,7 @@
+import {Injectable} from "angular2/angular2";
+
+// Injectable is not really necessary, just declares it for tools that might need it
+@Injectable()
 export class FoodService {
 	fruit = "apple";
 }
