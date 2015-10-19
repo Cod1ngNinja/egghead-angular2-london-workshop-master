2e009e46316f2fc652324ed4650f2450cdd4670d
Klas Mellbourn <klas@mellbourn.net>
2015-10-19 11:27:12 +0100

    nicer output

--- exercise/01-es6-and-ts/main.ts
+++ exercise/01-es6-and-ts/main.ts
@@ -5,4 +5,7 @@ var boardGame = new BoardGame("Dungeons and Dragons");
 
 var videoGame = new VideoGame("pacman");
 
-document.body.innerHTML = `${boardGame.play()} + ${videoGame.play()}` 
+document.body.innerHTML = `
+<div>${boardGame.play()}</div>
+<div>${videoGame.play()}</div>
+`;
