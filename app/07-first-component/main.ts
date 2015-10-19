11f4879cd381cbd1c5a23b651e9dcb159b4df0a2
Klas Mellbourn <klas@mellbourn.net>
2015-10-19 12:01:21 +0100

    secondComponent

--- app/07-first-component/main.ts
+++ app/07-first-component/main.ts
@@ -1,11 +1,15 @@
 import {Component, bootstrap} from "angular2/angular2";
 import {FirstComponent} from "./firstComponent";
+import {SecondComponent} from "./secondComponent";
 
 @Component({
 	selector: 'app',
-	directives: [FirstComponent],
+	directives: [FirstComponent, SecondComponent],
 	template: `
-	   <div><first-component></first-component></div>
+	   <div>
+	     <first-component></first-component>
+	     <second-component></second-component>
+	   </div>
 	`
 })
 class App{}
---
+++ app/07-first-component/secondComponent.ts
@@ -0,0 +1,11 @@
+import {Component} from "angular2/angular2";
+
+@Component({
+	selector:'second-component',
+	template:`
+	<div>
+	  This is the second component
+	</div>
+	`
+})
+export class SecondComponent{}
