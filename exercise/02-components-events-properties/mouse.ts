87856a1dc0f31775019e71719ffb7185fb0dc41b
Klas Mellbourn <klas@mellbourn.net>
2015-10-19 12:47:29 +0100

    handoff and handon events

--- exercise/02-components-events-properties/mouse.ts
+++ exercise/02-components-events-properties/mouse.ts
@@ -1,9 +1,21 @@
-import {Component, Input, Output, EventEmitter} from "angular2/angular2";
+import {Component, Output, EventEmitter} from "angular2/angular2";
 
 @Component({
 	selector: "mouse",
-	template: `I'm a mouse`
+	template: `<div (mouseout)="mouseout()" (mouseover)="mouseover()">I'm a mouse</div>`
 })
 export class Mouse {
+	@Output() handOff = new EventEmitter();
+	@Output() handOn = new EventEmitter();
 	constructor() {}
+	
+	mouseout() {
+		console.log("mouseout");
+		this.handOff.next({});
+	}
+	
+	mouseover() {
+		console.log("mouseover");
+		this.handOn.next({});
+	}
 }
