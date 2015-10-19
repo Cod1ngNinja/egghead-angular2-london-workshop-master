86731522451e0615cad72f60e4e15644cba6da4f
Klas Mellbourn <klas@mellbourn.net>
2015-10-19 16:39:13 +0100

    exercise better

--- exercise/04-services-and-ng-model/baseInput.ts
+++ exercise/04-services-and-ng-model/baseInput.ts
@@ -1,9 +1,10 @@
-import {Component} from "angular2/angular2";
+import {Component, FORM_DIRECTIVES} from "angular2/angular2";
 import {NumberService} from "./numberService";
 
 @Component({
 	selector: "base-input",
 	providers: [NumberService],
+	directives: [FORM_DIRECTIVES],
 	templateUrl: "baseInput.html"
 })
 export class BaseInput{
