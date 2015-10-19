6cb73cffd62bc697c2f4520d5cfd10310e91c464
Klas Mellbourn <klas@mellbourn.net>
2015-10-19 16:52:52 +0100

    added selector

--- app/11-ng-model-and-forms/simpleForm.ts
+++ app/11-ng-model-and-forms/simpleForm.ts
@@ -1,13 +1,18 @@
-import {Component, FORM_DIRECTIVES, ControlGroup, Control} from "angular2/angular2";
+import {Component, FORM_DIRECTIVES, ControlGroup, Control, CORE_DIRECTIVES} from "angular2/angular2";
 
 @Component({
 	selector:'simple-form',
-	directives: [FORM_DIRECTIVES],
+	directives: [FORM_DIRECTIVES, CORE_DIRECTIVES],
 	template:`
 	    <div>
 		  <form [ng-form-model]="movie" (ng-submit)="onSubmit()">
 		    <input type="text" ng-control="name">
 		    <input type="text" ng-control="two">
+			
+			<select ng-control="select">
+			  <option *ng-for="#o of selectOptions">{{o}}</option>
+			</select>
+			
 		    <input type="submit" value="Submit">
 			
 		  </form>
@@ -16,9 +21,12 @@ import {Component, FORM_DIRECTIVES, ControlGroup, Control} from "angular2/angula
 		`
 })
 export class SimpleForm{
+	selectOptions = ["fire", "earth", "wind"]
+	
 		movie = new ControlGroup({
 			name: new Control("Klas"),
-			two: new Control()
+			two: new Control(),
+			select: new Control(this.selectOptions[0])
 		})	
 		
 		onSubmit(){
